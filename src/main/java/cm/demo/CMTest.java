0aba8738c697f4e3ff4dccb2352b868689d19b42
il <ilmak@o2.pl>
2019-04-12 18:15:35 +0200

    Add objects in main method

--- src/main/java/cm/demo/CMTest.java
+++ src/main/java/cm/demo/CMTest.java
@@ -1,11 +1,16 @@
 package cm.demo;
 
 import cm.domain.CoffeeMachine;
+import cm.domain.CoffeeTank;
+import cm.domain.WaterTank;
 
 public class CMTest {
     public static void main(String[] args) throws InterruptedException {
 
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        CoffeeTank coffeeTank = new DemoCoffeeTank();
+        WaterTank waterTank = new DemoWaterTank();
+
+        CoffeeMachine coffeeMachine = new CoffeeMachine(waterTank,coffeeTank);
 
         coffeeMachine.coffeeButton();
         Thread.sleep(1000);
